344150f6242f7b86391e7a66d6f1db84ca28f6bd
Laksh Khandelwal <laksh.khandelwal@outlook.in>
2020-04-26 16:58:39 +0530

    Direct Reports Scripts
    AN Script to find all the peoples that are under a given person in AD Hierarchy

---
+++ PowerShell Scripts/DirectReports.ps1
@@ -0,0 +1,33 @@
+﻿
+Function Get-DirectReports{
+
+    [CmdletBinding()]Param([Parameter(Mandatory=$True)]  $SAMUserName)
+
+    $directreportslist = Get-ADUser $SAMUserName -Properties directreports | Select-Object -ExpandProperty directreports
+
+    foreach($item in $directreportslist){
+    $item;
+
+    $SamName = Get-ADUser $item | select SAMAccountName
+    
+    $directreportslisttemp = Get-ADUser $SamName.SAMAccountName -Properties directreports | Select-Object -ExpandProperty directreports
+
+        if($directreportslisttemp -eq $null){
+            
+            $SAMemployeeid = Get-ADUser $SamName.SAMAccountName -Properties employeeid | select employeeid 
+
+            if($SAMemployeeid.employeeid -ne $null){
+            $SamName.SAMAccountName|Out-File -FilePath C:\TEMP\DirectReports.txt -Append
+            }
+        }
+        else{
+            $SAMemployeeid = Get-ADUser $SamName.SAMAccountName -Properties employeeid | select employeeid 
+
+            if($SAMemployeeid.employeeid -ne $null){
+            $SamName.SAMAccountName|Out-File -FilePath C:\TEMP\DirectReports.txt -Append
+            }
+            Get-DirectReports -SAMUserName $SamName.SAMAccountName
+        }
+    }
+
+}
