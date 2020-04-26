23720b6d7dc873c84f7f189d33b1296e1d062484
Laksh Khandelwal <laksh.khandelwal@outlook.in>
2020-04-26 16:54:22 +0530

    AD Group Members Script
    A Script to find the AD Group Members of any group.

---
+++ PowerShell Scripts/ADGroupMembers.ps1
@@ -0,0 +1,15 @@
+﻿Function Get-GroupMembers{
+
+    [CmdletBinding()]Param([Parameter(Mandatory=$True)]  $GroupName)
+
+    $list_of_members = Get-ADGroupMember -Identity $GroupName
+
+    foreach($item in $list_of_members){
+        if($item.objectClass -eq "group"){
+            Get-GroupMembers -GroupName $item.name
+        }
+        elseif($item.objectClass -eq "user"){
+            $item.name|Out-File -FilePath C:\TEMP\userlist.txt -Append
+        }
+    }
+}
