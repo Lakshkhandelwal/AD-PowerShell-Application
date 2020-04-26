7754cdd9581dfed8a17028a6245a067563a385c7
Laksh Khandelwal <laksh.khandelwal@outlook.in>
2020-04-26 16:56:41 +0530

    AD Multiple Group's Members Retriever Script
    An Script which when provided with the list of group names in a CSV file, finds the list of all the Members in all the groups

---
+++ PowerShell Scripts/AD_groupMembers_CSV.ps1
@@ -0,0 +1,25 @@
+﻿$csvfile = Import-Csv -Path .\groupslist.csv;
+
+Function Get-GroupMembers{
+
+    [CmdletBinding()]Param([Parameter(Mandatory=$True)]  $GroupName)
+
+    $list_of_members = Get-ADGroupMember -Identity $GroupName;
+     $GroupName|Out-File -FilePath C:\TEMP\userlist.txt -Append
+     " "|Out-File -FilePath C:\TEMP\userlist.txt -Append;
+
+    foreach($item in $list_of_members){
+        if($item.objectClass -eq "group"){
+            Get-GroupMembers -GroupName $item.name
+        }
+        elseif($item.objectClass -eq "user"){
+            $item.name|Out-File -FilePath C:\TEMP\userlist.txt -Append
+        }
+    }
+    " "|Out-File -FilePath C:\TEMP\userlist.txt -Append;
+    " "|Out-File -FilePath C:\TEMP\userlist.txt -Append;
+}
+
+foreach($element in $csvfile){
+Get-GroupMembers -GroupName $element.GroupsList
+}
